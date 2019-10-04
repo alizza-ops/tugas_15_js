bbc6311b85016e12ee2bcd810877f50f5d9d9017
alizza-ops <52742283+alizza-ops@users.noreply.github.com>
2019-10-04 23:38:23 +0700

    Add files via upload

---
+++ tugas15.js
@@ -0,0 +1,14 @@
+function pitagoras(){
+
+	var ab=8,bc=6;
+	console.log("nilai AB: "+ab);
+	console.log("nilai BC: "+bc);
+
+	x = Math.pow(ab,2);
+	y = Math.pow(bc,2);
+
+	console.log(Math.sqrt(x+y));
+}
+
+pitagoras();
+
